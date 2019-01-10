638aaebaf909f3c00ec5a86f2e86ad4742561698
Tigertronics <team2053tigertronics@gmail.com>
2019-01-10 18:36:02 -0500

    more edits drew made
    whatever changes drew made, i needed to restart the computer

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -2,13 +2,18 @@
 
 #include <frc/commands/Scheduler.h>
 #include <frc/smartdashboard/SmartDashboard.h>
+#include <rev/CANSparkMax.h>
+#include <rev/CANSparkMaxLowLevel.h>
 
 std::unique_ptr<DrivebaseSubsystem> Robot::drivebaseSubsystem;
 std::unique_ptr<OI> Robot::oi;
 
+std::unique_ptr<rev::CANSparkMax> Robot::test;
+
 void Robot::RobotInit() {
   drivebaseSubsystem = std::make_unique<DrivebaseSubsystem>();
   oi = std::make_unique<OI>();
+  test = std::make_unique<rev::CANSparkMax>(4, rev::CANSparkMaxLowLevel::MotorType::kBrushless);
 }
 
 void Robot::RobotPeriodic() {
@@ -24,10 +29,14 @@ void Robot::DisabledPeriodic() {
 }
 
 void Robot::AutonomousInit() {
-
+  test->Set(.5);
 }
 
-void Robot::AutonomousPeriodic() { frc::Scheduler::GetInstance()->Run(); }
+void Robot::AutonomousPeriodic() { 
+  frc::Scheduler::GetInstance()->Run();
+  SmartDashboard::PutNumber("ENCODER BRUSHLESS POS", test->GetEncoder().GetPosition());
+  SmartDashboard::PutNumber("ENCODER BRUSHLESS VEL", test->GetEncoder().GetVelocity());
+ }
 
 void Robot::TeleopInit() {
 
